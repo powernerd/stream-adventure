77d8714d7af65a45532b936d286d7799d2d81721
Andy <rsaman@powernerdsoftware.com>
2017-08-05 15:44:45 -0700

    #5 lines

--- program.js
+++ program.js
@@ -1,13 +1,18 @@
-var through = require('through2');
-var stream = through(write, end);
+var split = require('split')
+var through = require('through2')
 
-function write (buffer, encoding, next) {
-  this.push(buffer.toString().toUpperCase());
-  next();
-}
-function end (done) {
-  done();
-}
+var lineCount = 0
+var stream = through(function (buffer, encoding, next) {
+  var line = buffer.toString()
+  this.push(lineCount % 2 === 0
+    ? line.toLowerCase() + '\n'
+    : line.toUpperCase() + '\n'
+  )
+  lineCount++
+  next()
+})
 
-
-process.stdin.pipe(stream).pipe(process.stdout);
+process.stdin
+  .pipe(split())
+  .pipe(stream)
+  .pipe(process.stdout)
